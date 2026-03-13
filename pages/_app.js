ba8d069a830c5e6c6334a5d93f300847aa716e27
jamjotaku <156283662+jamjotaku@users.noreply.github.com>
2026-03-13 06:47:32 +0000

    vercelデプロイ修正

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import '../styles/globals.css' // 既存のグローバルCSS
+// import '../styles/globals.css'  // ← ここが原因。ファイルがない場合はコメントアウト
 import GlobalCommander from '../components/GlobalCommander'
 
 function MyApp({ Component, pageProps }) {
